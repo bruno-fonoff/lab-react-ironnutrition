bdd00e319e7c7c5da1ba1c58ee28f626ff40c7a1
bruno-fonoff <105615381+bruno-fonoff@users.noreply.github.com>
2023-11-11 01:29:07 -0300

    iteration 3 Done

--- src/App.js
+++ src/App.js
@@ -7,23 +7,13 @@ function App() {
   const [allFoods, setAllFoods] = useState(foods);
   return (
     <div className="App">
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
-      {/* ITERATION 1 */}
-      {/* {allFoods.map((food) => {
+      {allFoods.map((food) => {
         return (
           <div>
-            <p> {food.name} </p>
-            <img src={food.image} width={100} />
+            <FoodBox food={food} />
           </div>
         );
-      })} */}
+      })}
     </div>
   );
 }
