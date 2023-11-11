0af84613dce24a462f0636cdb4e42713ecce86ac
bruno-fonoff <105615381+bruno-fonoff@users.noreply.github.com>
2023-11-11 01:14:21 -0300

    Iterations 0-1 Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [allFoods, setAllFoods] = useState(foods);
+  return (
+    <div className="App">
+      {allFoods.map((food) => {
+        return (
+          <div>
+            <p> {food.name} </p>
+            <img src={food.image} width={100} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
